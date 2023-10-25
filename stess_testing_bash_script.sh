83cfcfbdbd1039f9890c59f56abdb1a82197e554
AJF_Shuvo <ajfshuvo98@gmail.com>
2023-10-25 16:21:38 +0600

    automate testcases

---
+++ stess_testing_bash_script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+# To color the output text in different colours.
+green=$(tput setaf 71);
+red=$(tput setaf 1);
+blue=$(tput setaf 32);
+orange=$(tput setaf 178);
+bold=$(tput bold);
+reset=$(tput sgr0);
+
+echo "Compiling Codes"
+
+g++ -std=gnu++17 -O2 -Wall -o test_gen test_gen.cpp
+g++ -std=gnu++17 -O2 -Wall -o sol sol.cpp
+g++ -std=gnu++17 -O2 -Wall -o brute brute.cpp
+
+for ((i = 1; ; i++)); do
+    ./test_gen > input.txt
+    if cmp -s <(./sol < input.txt) <(./brute < input.txt); then
+      echo "${orange}test_case #$i: ${bold}${green}Accepted${reset}"
+    else
+      echo "${orange}test_case #$i: ${bold}${red}Wrong Answer${reset}"
+      echo "${blue}Input: ${reset}"
+        cat input.txt
+      echo ""
+    break
+  fi
+done
